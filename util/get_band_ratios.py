371f29da3f780d644589e603d3ce96d407404447
ianzyong <ianzyong@gmail.com>
2023-02-27 02:37:38 -0500

    sleep/wake classification

--- util/get_band_ratios.py
+++ util/get_band_ratios.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import scipy
 from fooof import FOOOF
+import time
 
 def convertSeconds(time): 
     seconds = time % 3600 
@@ -19,6 +20,12 @@ def convertSeconds(time):
     if hours < 10:
         hours = "0" + str(hours)
     return ":".join(str(n) for n in [hours,minutes,seconds])
+
+def convertTimeStamp(time):
+    secs = time.second
+    mins = time.minute
+    hours = time.hour
+    return hours*60*60 + mins*60 + secs
 
 figure_saved = False
 
@@ -36,6 +43,20 @@ with open(args.password, 'r') as f:
 
 dataset_name = args.iEEG_filename
 
+# read file with start times
+start_times = pd.read_excel("start_times.xlsx")
+
+# get real start time in seconds
+name_parts = dataset_name.split("_")
+if (len(name_parts) < 3):
+    timestamp = start_times.loc[start_times['name'] == name_parts[0],1].values[0]
+else:
+    timestamp = start_times.loc[start_times['name'] == name_parts[0],int(name_parts[2][-1])].values[0]
+
+offset_seconds = convertTimeStamp(timestamp)
+
+print(f"Actual start time = {timestamp} = {offset_seconds} seconds")
+
 # get info from patient recording
 with Session(args.user, pwd) as session:
     ds = session.open_dataset(dataset_name)
@@ -45,6 +66,9 @@ with Session(args.user, pwd) as session:
     session.close_dataset(dataset_name)
     
 all_ratios = []
+
+# start timer
+total_start = time.time()
 
 # iterate through time clips
 for start in range(0,int(duration),int(args.interval_spacing_usec)):
@@ -96,20 +120,53 @@ for start in range(0,int(duration),int(args.interval_spacing_usec)):
     all_ratios.append(ad_ratios)
 
 all_ratios = np.hstack(all_ratios)
+
+# remove EKG channels
+not_EKG = ["EKG" not in x for x in labels]
+#print(not_EKG)
+filter_inds = np.ravel(np.argwhere(not_EKG))
+#print(filter_inds)
+all_ratios = np.squeeze(all_ratios[filter_inds,:])
+print(f"{np.sum(['EKG' in x for x in labels])} EKG channels removed.")
+labels = [label for label in labels if "EKG" not in label]
+print(labels)
+
+print("Calculated ratios:")
 print(all_ratios.shape)
 print(all_ratios)
 
+np.save(f"../data/{dataset_name}_ratios",all_ratios)
+total_end = time.time()
+
+print("Ratios saved to file. Intervals processed in {}.".format(convertSeconds(int(total_end - total_start))))
+
+# determine sleep/wake
+# average across channels
+ad = np.nanmean(all_ratios, axis=0)
+# normalize across times
+norm_ad = np.divide((ad-np.nanmedian(ad)),scipy.stats.iqr(ad))
+disc = -0.4054
+# classify
+is_awake = norm_ad > disc
+
+print("Sleep/wake classification:")
+print(is_awake)
+
+# sleep/wake colors
+swc = ["#f5e642" if x else "#0005a1" for x in is_awake]
+
 # plot result
 plt.imshow(all_ratios, extent=[0,all_ratios.shape[1],0,all_ratios.shape[0]], cmap='hot', interpolation='nearest')
-plt.xlabel("Window Start (s)")
-plt.ylabel("Channel")
-plt.xticks(range(int(duration)//int(args.interval_spacing_usec)+1),labels=[convertSeconds(x) for x in range(int(0),int(duration/1000000),int(args.interval_spacing_usec/1000000))], fontsize=4, rotation = "vertical")
-plt.yticks(range(len(labels)),labels=labels, fontsize=4)
-plt.title(f"Patient = {args.iEEG_filename}")
 cbar = plt.colorbar()
 cbar.set_label("Alpha to Delta Band Power Ratio")
-#plt.tight_layout()
+plt.scatter(range(len(is_awake)),np.zeros((1,len(is_awake))),c=swc)
+plt.xlabel("Time of day (s)")
+plt.ylabel("Channel")
+plt.xticks(range(int(duration)//int(args.interval_spacing_usec)+1),labels=[convertSeconds(offset_seconds+x) for x in range(int(0),int(duration/1000000),int(args.interval_spacing_usec/1000000))], fontsize=4, rotation = "vertical")
+print(len(labels))
+plt.yticks(range(len(labels)),labels=labels, fontsize=4)
+plt.title(f"Patient = {args.iEEG_filename}")
 fig = plt.gcf()
 fig.set_size_inches(40, 8)
-plt.savefig("heatmap.png",bbox_inches="tight",dpi=600)
+plt.savefig(f"{dataset_name}_heatmap.png",bbox_inches="tight",dpi=600)
 print("Figure saved to file.")
